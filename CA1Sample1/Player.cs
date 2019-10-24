192b36de5d2807150ff99a733db36788f1ac7dc6
Hannah Byrne <S00189764@mail.itsligo.ie>
2019-10-24 11:11:50 +0100

    Add project files.

---
+++ CA1Sample1/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1Sample1
+{
+    class Player
+    {
+        // Question 2
+        private int PlayerID { get; set; }
+        private string PlayerName { get; set; }
+        private int Score { get; set; }
+    }
+}
